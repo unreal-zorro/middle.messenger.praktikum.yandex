0af5fe250e60a6218dfe04ad5dad01c7fde49c02
unreal-zorro <unrealzorro@gmail.com>
2024-03-02 16:18:10 +0300

    add login-page

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { Button } from './button';
+export { Error } from './error';
+export { Form } from './form';
+export { FormControl } from './form-control';
+export { Header } from './header';
+export { Input } from './input';
+export { Label } from './label';
+export { Link } from './link';
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import './style.scss';
 import Handlebars from 'handlebars';
+import * as Components from './components';
+import * as Pages from './pages';
 
+const pages = {
+  'login': [ Pages.LoginPage ]
+};
 
+Object.entries(Components).forEach(([ name, component ]) =>{
+  Handlebars.registerPartial(name, component);
+});
+
+function navigate(page) {
+  const [ source, args ] = pages[page];
+  const template = Handlebars.compile(source);
+  const root = document.querySelector('#root');
+  root.innerHTML = template(args);
+}
+
+const contentLoadedHandler = () => {
+  navigate('login');
+}
+
+document.addEventListener('DOMContentLoaded', contentLoadedHandler);
