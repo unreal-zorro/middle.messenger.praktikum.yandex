039f0250a506d838bc62fff3d79c4d037b2bb9e0
unreal-zorro <unrealzorro@gmail.com>
2024-03-03 21:00:06 +0300

    add register-page and edit login-page

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const PORT = 3000;
-
 const app = express();
+const root = path.join(__dirname, './dist')
 
-app.use(express.static(path.resolve(__dirname, './dist')));
+app.use(express.static(root));
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root });
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- src/main.js
+++ src/main.js
@@ -4,22 +4,37 @@ import * as Components from './components';
 import * as Pages from './pages';
 
 const pages = {
-  'login': [ Pages.LoginPage ]
+  login: [Pages.LoginPage],
+  register: [Pages.RegisterPage]
 };
 
-Object.entries(Components).forEach(([ name, component ]) =>{
+Object.entries(Components).forEach(([name, component]) => {
   Handlebars.registerPartial(name, component);
 });
 
 function navigate(page) {
-  const [ source, args ] = pages[page];
+  const [source, args] = pages[page];
   const template = Handlebars.compile(source);
   const root = document.querySelector('#root');
   root.innerHTML = template(args);
 }
 
 const contentLoadedHandler = () => {
-  navigate('login');
-}
+  const pathname = window.location.pathname;
+  let page = 'login';
+
+  switch (pathname) {
+    case '/':
+    case '/index.html':
+      break;
+    case '/register':
+      page = 'register';
+      break;
+    default:
+      break;
+  }
+
+  navigate(page);
+};
 
 document.addEventListener('DOMContentLoaded', contentLoadedHandler);
