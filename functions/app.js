f240158913e4a4f1053d6638fcc114770c56b21c
unreal-zorro <55550188+unreal-zorro@users.noreply.github.com>
2024-03-03 23:21:12 +0300

    update netlify function app.js

--- functions/app.js
+++ functions/app.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const root = path.join(__dirname, '../', '../', './dist');
+const root = path.join(__dirname, './dist');
 
 app.use(express.static(root));
 
