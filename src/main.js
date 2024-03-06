52c12d201a6ca112aa5a39b55272e297748b8216
unreal-zorro <unrealzorro@gmail.com>
2024-03-06 21:01:04 +0300

    refactor profile page

--- src/main.js
+++ src/main.js
@@ -267,111 +267,229 @@ const getRegisterPageData = (user) => {
   };
 };
 
-const getProfilePageData = (user) => {
-  return {
-    id: 'profile',
-    header: user.chat_name,
-    avatar: user.avatar,
-    controls: [
-      {
-        label: 'Почта',
-        name: 'email',
-        type: 'email',
-        disabled: true,
-        placeholder: '',
-        value: user.email,
-        error: 'Неверная почта'
-      },
-      {
-        label: 'Логин',
-        name: 'login',
-        type: 'text',
-        disabled: true,
-        placeholder: '',
-        value: user.login,
-        error: 'Неверный логин'
-      },
-      {
-        label: 'Имя',
-        name: 'first_name',
-        type: 'text',
-        disabled: true,
-        placeholder: '',
-        value: user.first_name,
-        error: 'Неверное имя'
-      },
-      {
-        label: 'Фамилия',
-        name: 'second_name',
-        type: 'text',
-        disabled: true,
-        placeholder: '',
-        value: user.first_name,
-        error: 'Неверная фамилия'
-      },
-      {
-        label: 'Имя в чате',
-        name: 'chat_name',
-        type: 'text',
-        disabled: true,
-        placeholder: '',
-        value: user.chat_name,
-        error: 'Неверное имя в чате'
-      },
-      {
-        label: 'Телефон',
-        name: 'phone',
-        type: 'tel',
-        disabled: '',
-        placeholder: '',
-        value: user.first_name,
-        error: 'Неверный телефон'
-      },
-      {
-        label: 'Старый пароль',
-        name: 'password',
-        type: 'password',
-        disabled: true,
-        placeholder: '',
-        value: user.password,
-        error: 'Неверный пароль'
-      },
-      {
-        label: 'Новый пароль',
-        name: 'new_password',
-        type: 'password',
-        disabled: true,
-        placeholder: '',
-        value: '1234567891011121',
-        error: 'Пароли не совпадают'
-      },
-      {
-        label: 'Повторите пароль ещё раз',
-        name: 'password_again',
-        type: 'password',
-        disabled: true,
-        placeholder: '',
-        value: '1234567891011122',
-        error: 'Пароли не совпадают'
+const getProfilePageData = (user, mode) => {
+  let data = {};
+
+  if (mode === 'edit') {
+    data = {
+      id: 'profile-edit',
+      avatar: user.avatar,
+      header: user.chat_name,
+      controls: [
+        {
+          label: 'Почта',
+          name: 'email',
+          type: 'email',
+          disabled: false,
+          placeholder: '',
+          value: user.email,
+          error: ''
+        },
+        {
+          label: 'Логин',
+          name: 'login',
+          type: 'text',
+          disabled: false,
+          placeholder: '',
+          value: user.login,
+          error: ''
+        },
+        {
+          label: 'Имя',
+          name: 'first_name',
+          type: 'text',
+          disabled: false,
+          placeholder: '',
+          value: user.first_name,
+          error: ''
+        },
+        {
+          label: 'Фамилия',
+          name: 'second_name',
+          type: 'text',
+          disabled: false,
+          placeholder: '',
+          value: user.second_name,
+          error: ''
+        },
+        {
+          label: 'Имя в чате',
+          name: 'chat_name',
+          type: 'text',
+          disabled: false,
+          placeholder: '',
+          value: user.chat_name,
+          error: ''
+        },
+        {
+          label: 'Телефон',
+          name: 'phone',
+          type: 'tel',
+          disabled: false,
+          placeholder: '',
+          value: user.phone,
+          error: ''
+        }
+      ],
+      buttons: [
+        {
+          type: 'submit',
+          text: 'Сохранить',
+          href: '/profile'
+        }
+      ],
+      link: {}
+    };
+  } else if (mode === 'password') {
+    data = {
+      id: 'profile-password',
+      avatar: user.avatar,
+      controls: [
+        {
+          label: 'Старый пароль',
+          name: 'password',
+          type: 'password',
+          disabled: false,
+          placeholder: '',
+          value: user.password,
+          error: ''
+        },
+        {
+          label: 'Новый пароль',
+          name: 'new_password',
+          type: 'password',
+          disabled: false,
+          placeholder: '',
+          value: '1234567891011121',
+          error: ''
+        },
+        {
+          label: 'Повторите пароль ещё раз',
+          name: 'password_again',
+          type: 'password',
+          disabled: false,
+          placeholder: '',
+          value: '1234567891011122',
+          error: ''
+        }
+      ],
+      buttons: [
+        {
+          type: 'submit',
+          text: 'Сохранить',
+          href: '/profile'
+        }
+      ],
+      link: {}
+    };
+  } else {
+    data = {
+      id: 'profile',
+      avatar: user.avatar,
+      header: user.chat_name,
+      controls: [
+        {
+          label: 'Почта',
+          name: 'email',
+          type: 'email',
+          disabled: true,
+          placeholder: '',
+          value: user.email,
+          error: ''
+        },
+        {
+          label: 'Логин',
+          name: 'login',
+          type: 'text',
+          disabled: true,
+          placeholder: '',
+          value: user.login,
+          error: ''
+        },
+        {
+          label: 'Имя',
+          name: 'first_name',
+          type: 'text',
+          disabled: true,
+          placeholder: '',
+          value: user.first_name,
+          error: ''
+        },
+        {
+          label: 'Фамилия',
+          name: 'second_name',
+          type: 'text',
+          disabled: true,
+          placeholder: '',
+          value: user.second_name,
+          error: ''
+        },
+        {
+          label: 'Имя в чате',
+          name: 'chat_name',
+          type: 'text',
+          disabled: true,
+          placeholder: '',
+          value: user.chat_name,
+          error: ''
+        },
+        {
+          label: 'Телефон',
+          name: 'phone',
+          type: 'tel',
+          disabled: true,
+          placeholder: '',
+          value: user.phone,
+          error: ''
+        },
+        {
+          label: 'Старый пароль',
+          name: 'password',
+          type: 'password',
+          disabled: true,
+          placeholder: '',
+          value: user.password,
+          error: ''
+        },
+        {
+          label: 'Новый пароль',
+          name: 'new_password',
+          type: 'password',
+          disabled: true,
+          placeholder: '',
+          value: '1234567891011121',
+          error: ''
+        },
+        {
+          label: 'Повторите пароль ещё раз',
+          name: 'password_again',
+          type: 'password',
+          disabled: true,
+          placeholder: '',
+          value: '1234567891011122',
+          error: ''
+        }
+      ],
+      buttons: [
+        {
+          type: 'button',
+          text: 'Изменить данные',
+          href: '/profile/edit-data'
+        },
+        {
+          type: 'button',
+          text: 'Изменить пароль',
+          href: '/profile/edit-password'
+        }
+      ],
+      link: {
+        text: 'Выйти',
+        href: '/'
       }
-    ],
-    buttons: [
-      {
-        type: 'button',
-        text: 'Изменить данные',
-        href: '/profile/edit-data'
-      },
-      {
-        type: 'button',
-        text: 'Изменить пароль',
-        href: '/profile/edit-password'
-      }
-    ],
-    link: {
-      text: 'Выйти',
-      href: '/'
-    }
-  };
+    };
+  }
+
+  return data;
 };
 
 function navigate(page, args = {}) {
@@ -411,9 +529,21 @@ const contentLoadedHandler = () => {
       page = 'error500';
       break;
     }
-    case '/profile':
+    case '/profile': {
+      data = getProfilePageData(user);
       page = 'profile';
       break;
+    }
+    case '/profile/edit': {
+      data = getProfilePageData(user, 'edit');
+      page = 'profile';
+      break;
+    }
+    case '/profile/password': {
+      data = getProfilePageData(user, 'password');
+      page = 'profile';
+      break;
+    }
     case '/chats':
       page = 'chats';
       break;
