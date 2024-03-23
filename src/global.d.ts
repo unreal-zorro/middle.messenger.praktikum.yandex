3d903534f107ab8bf6b63c53b5cb157d81221ea8
unreal-zorro <unrealzorro@gmail.com>
2024-03-23 15:27:58 +0300

    edit README.md

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,2 @@
-declare module '*.scss' {
-  type IClassNames = Record<string, string>;
-  const classNames: IClassNames;
-  export = classNames;
-}
-
 type Nullable<T> = T | null;
 type ReverseMap<T> = T[keyof  T];
