36d8b6cf2bba4e6703e64312da709a1024675bde
unreal-zorro <unrealzorro@gmail.com>
2024-04-09 20:26:52 +0300

    add chat model

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export type { LoginFormModel } from './login-form-model';
 export type { RegisterFormModel } from './register-form-model';
 export type { UserModel } from './user-model';
 export type { PasswordModel } from './password-model';
+export type { ChatModel } from './chat-model';
