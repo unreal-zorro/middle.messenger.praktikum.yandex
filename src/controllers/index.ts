1ab08bac97af5baac24fb19c42b887490ccaefaf
unreal-zorro <unrealzorro@gmail.com>
2024-04-11 07:01:50 +0300

    update index.ts in controllers and models

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export { UserController } from './user-controller';
 export { UserLoginController } from './user-login-controller';
 export { AuthController } from './auth-controller';
 export { ChatController } from './chat-controller';
+export { ChatUsersController } from './chat-users-controller';
--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,4 @@ export type { RegisterFormModel } from './register-form-model';
 export type { UserModel } from './user-model';
 export type { PasswordModel } from './password-model';
 export type { ChatModel } from './chat-model';
+export type { ChatUserModel } from './chat-user-model';
