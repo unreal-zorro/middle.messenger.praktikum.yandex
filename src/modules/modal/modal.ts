2c16093ae25cc3a3938dd10a032903bf8e4745ca
unreal-zorro <unrealzorro@gmail.com>
2024-04-13 15:49:08 +0300

    add state props in modal

--- src/modules/modal/modal.ts
+++ src/modules/modal/modal.ts
@@ -15,6 +15,7 @@ export interface ModalProps extends Props {
   controls?: InputFieldProps[];
   buttons?: ButtonProps[];
   visible?: boolean;
+  state?: ModalProps;
   submitHandler?: Listener;
   closeHandler?: Listener;
 }
@@ -22,175 +23,183 @@ export interface ModalProps extends Props {
 export class Modal extends Block {
   constructor(props: ModalProps) {
     super(props);
+  }
 
-    const focusHandler = () => {
+  public focusHandler = () => {
+    (this.children.buttons as Block[])?.map((button) =>
+      button.setProps({
+        disabled: true
+      })
+    );
+  };
+
+  public blurHandler: (...args: string[]) => string = (name, value) => {
+    const { regExp } = VALIDATION_RULES[name];
+    const isValid = regExp.test(value);
+
+    if (!isValid) {
       (this.children.buttons as Block[])?.map((button) =>
         button.setProps({
           disabled: true
         })
       );
-    };
 
-    const blurHandler: (...args: string[]) => string = (name, value) => {
-      const { regExp } = VALIDATION_RULES[name];
-      const isValid = regExp.test(value);
+      const { message } = VALIDATION_RULES[name];
+      return message;
+    }
 
-      if (!isValid) {
-        (this.children.buttons as Block[])?.map((button) =>
-          button.setProps({
-            disabled: true
-          })
-        );
+    (this.children.buttons as Block[])?.map((button) =>
+      button.setProps({
+        disabled: false
+      })
+    );
 
-        const { message } = VALIDATION_RULES[name];
-        return message;
-      }
+    this._formData[name] = value;
 
-      (this.children.buttons as Block[])?.map((button) =>
-        button.setProps({
-          disabled: false
-        })
-      );
+    return '';
+  };
 
-      this._formData[name] = value;
-
-      return '';
-    };
-
-    const changeHandler: (...args: string[]) => string = (name, value, type) => {
-      if (!type.includes('image') && this.props.type === 'image') {
-        (this.children.buttons as Block[])?.forEach((button) =>
-          button.setProps({
-            disabled: true
-          })
-        );
-
-        (this.children.controls as Block[])?.forEach((control) =>
-          control.setProps({
-            classNameLabel: 'modal__label modal__label-file'
-          })
-        );
-
-        const message = 'Нужно выбрать файл с изображением';
-        return message;
-      }
-
+  public changeHandler: (...args: string[]) => string = (name, value, type) => {
+    if (!type.includes('image') && this.props.type === 'image') {
       (this.children.buttons as Block[])?.forEach((button) =>
         button.setProps({
-          disabled: false
+          disabled: true
         })
       );
 
       (this.children.controls as Block[])?.forEach((control) =>
         control.setProps({
-          classNameLabel: 'modal__label modal__label-file modal__label-selected'
+          classNameLabel: 'modal__label modal__label-file'
         })
       );
 
-      this._formData[name] = value;
+      const message = 'Нужно выбрать файл с изображением';
+      return message;
+    }
 
-      return '';
-    };
+    (this.children.buttons as Block[])?.forEach((button) =>
+      button.setProps({
+        disabled: false
+      })
+    );
 
-    const submitHandler: (event: SubmitEvent) => void = (event) => {
-      event.preventDefault();
+    (this.children.controls as Block[])?.forEach((control) =>
+      control.setProps({
+        classNameLabel: 'modal__label modal__label-file modal__label-selected'
+      })
+    );
 
-      (this.children.controls as InputField[]).forEach((control) => {
-        const element = control.getContent()?.querySelector('input') as HTMLInputElement;
-        const elementName = element?.getAttribute('name') as string;
-        const elementValue = element?.getAttribute('value') as string;
-        const elementType = element?.getAttribute('type') as string;
+    this._formData[name] = value;
 
-        if (elementType === 'file') {
-          if (element.files && element.files.length !== 0) {
-            const type = element.files[0]?.type;
-            const data = element.files[0];
+    return '';
+  };
 
-            if (!type.includes('image') && this.props.type === 'image') {
-              return;
-            }
+  public submitHandler: (event: SubmitEvent) => void = (event) => {
+    event.preventDefault();
 
-            this._formData[elementName] = data;
+    (this.children.controls as InputField[]).forEach((control) => {
+      const element = control.getContent()?.querySelector('input') as HTMLInputElement;
+      const elementName = element?.getAttribute('name') as string;
+      const elementValue = element?.getAttribute('value') as string;
+      const elementType = element?.getAttribute('type') as string;
+
+      if (elementType === 'file') {
+        if (element.files && element.files.length !== 0) {
+          const type = element.files[0]?.type;
+          const data = element.files[0];
+
+          if (!type.includes('image') && this.props.type === 'image') {
+            return;
           }
-        } else {
-          this._formData[elementName] = elementValue;
+
+          this._formData[elementName] = data;
+        }
+      } else {
+        this._formData[elementName] = elementValue;
+      }
+    });
+
+    (this.children.buttons as Block[])?.forEach((button) =>
+      button.setProps({
+        disabled: true
+      })
+    );
+
+    (this.children.controls as Block[])?.forEach((control) =>
+      control.setProps({
+        classNameLabel: 'modal__label'
+      })
+    );
+
+    if (this.props.submitHandler) {
+      (this.props.submitHandler as Listener<Record<string, string | File>>)(this._formData);
+    }
+  };
+
+  public closeHandler: (event: SubmitEvent) => void = (event) => {
+    event.preventDefault();
+
+    if (this.props.closeHandler) {
+      (this.props.closeHandler as Listener)();
+    }
+  };
+
+  public initControls() {
+    this.children.controls = (this.props.controls as InputFieldProps[])?.map((_control, index) => {
+      const type = ((this.props.state as ModalProps)?.controls as InputFieldProps[])?.[index].type;
+      const name = ((this.props.state as ModalProps)?.controls as InputFieldProps[])?.[index].name;
+      const label = ((this.props.state as ModalProps)?.controls as InputFieldProps[])?.[index]
+        .label;
+      const value = ((this.props.state as ModalProps)?.controls as InputFieldProps[])?.[index]
+        .value;
+      const error = ((this.props.state as ModalProps)?.controls as InputFieldProps[])?.[index]
+        .error;
+      const text = ((this.props.state as ModalProps)?.controls as InputFieldProps[])?.[index].text;
+
+      return new InputField({
+        className: `modal__input-field ${type === 'file' ? 'modal__input-field-file' : ''}`,
+        classNameLabel: `modal__label ${type === 'file' ? 'modal__label-file' : ''}`,
+        classNameInput: `modal__input ${type === 'file' ? 'modal__input-file' : ''}`,
+        classNameError: `modal__error ${type === 'file' ? 'modal__error-file' : ''}`,
+        name,
+        label,
+        type,
+        value,
+        placeholder: '',
+        disabled: false,
+        error,
+        text,
+        focusHandler: this.focusHandler,
+        blurHandler: this.blurHandler,
+        changeHandler: this.changeHandler,
+        settings: {
+          withInternalID: true
         }
       });
+    });
+  }
 
-      (this.children.buttons as Block[])?.forEach((button) =>
-        button.setProps({
-          disabled: true
-        })
-      );
+  public initButtons() {
+    this.children.buttons = (this.props.buttons as ButtonProps[])?.map((_button, index) => {
+      const type = ((this.props.state as ModalProps)?.buttons as ButtonProps[])?.[index].type;
+      const text = ((this.props.state as ModalProps)?.buttons as ButtonProps[])?.[index].text;
 
-      (this.children.controls as Block[])?.forEach((control) =>
-        control.setProps({
-          classNameLabel: 'modal__label'
-        })
-      );
+      return new Button({
+        className: 'modal__button',
+        type,
+        text,
+        disabled: true,
+        settings: {
+          withInternalID: false
+        },
+        events: {
+          click: ((event: SubmitEvent) => this.submitHandler.call(this, event)) as Listener
+        }
+      });
+    });
+  }
 
-      if (this.props.submitHandler) {
-        (this.props.submitHandler as Listener<Record<string, string | File>>)(this._formData);
-      }
-    };
-
-    const closeHandler: (event: SubmitEvent) => void = (event) => {
-      event.preventDefault();
-
-      (this.children.controls as Block[])?.forEach((control) =>
-        control.setProps({
-          classNameLabel: 'modal__label modal__label-file',
-          label: 'Выбрать файл на компьютере'
-        })
-      );
-
-      if (this.props.closeHandler) {
-        (this.props.closeHandler as Listener)();
-      }
-    };
-
-    this.children.controls = (this.props.controls as InputFieldProps[])?.map(
-      (control) =>
-        new InputField({
-          className: `modal__input-field ${
-            control.type === 'file' ? 'modal__input-field-file' : ''
-          }`,
-          classNameLabel: `modal__label ${control.type === 'file' ? 'modal__label-file' : ''}`,
-          classNameInput: `modal__input ${control.type === 'file' ? 'modal__input-file' : ''}`,
-          classNameError: `modal__error ${control.type === 'file' ? 'modal__error-file' : ''}`,
-          name: control.name,
-          label: control.label,
-          type: control.type,
-          value: control.value,
-          placeholder: '',
-          disabled: false,
-          error: control.error,
-          text: control.text,
-          focusHandler,
-          blurHandler,
-          changeHandler,
-          settings: {
-            withInternalID: true
-          }
-        })
-    );
-
-    this.children.buttons = (this.props.buttons as ButtonProps[])?.map(
-      (button) =>
-        new Button({
-          className: 'modal__button',
-          type: button.type,
-          text: button.text,
-          disabled: true,
-          settings: {
-            withInternalID: false
-          },
-          events: {
-            click: ((event: SubmitEvent) => submitHandler.call(this, event)) as Listener
-          }
-        })
-    );
-
+  public initCloseButton() {
     this.children.closeButton = new Button({
       dataButton: 'closeButton',
       className: 'modal__close',
@@ -206,9 +215,19 @@ export class Modal extends Block {
         href: '#icon-delete'
       }),
       events: {
-        click: ((event: SubmitEvent) => closeHandler.call(this, event)) as Listener
+        click: ((event: SubmitEvent) => this.closeHandler.call(this, event)) as Listener
       }
     });
+  }
+
+  async componentDidMount() {
+    try {
+      this.initControls();
+      this.initButtons();
+      this.initCloseButton();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   _formData: Record<string, string | File> = {};
