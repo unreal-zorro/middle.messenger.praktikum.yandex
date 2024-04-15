ad2214dde8a2ec1e3936a1019a1cf06988d9e902
unreal-zorro <unrealzorro@gmail.com>
2024-04-15 21:00:43 +0300

    update messages-api

--- src/api/messages-api/messages-api.ts
+++ src/api/messages-api/messages-api.ts
@@ -16,14 +16,14 @@ class MessagesAPI {
     this._baseURL = baseURL;
     this._baseWSSURL = baseWSSURL;
 
-    this.httpTransport = new HTTPTransport(`${this._baseURL}/chats`);
+    this.httpTransport = new HTTPTransport(`${this._baseURL}/chats/token`);
   }
 
   // Get token
   private async _getToken(id: number): Promise<void> {
     try {
-      const token = await this.httpTransport.get(`/${id}`);
-      this._token = token;
+      const data = await this.httpTransport.post(`/${id}`);
+      this._token = data.token;
     } catch (error) {
       console.log(error);
     }
@@ -33,7 +33,6 @@ class MessagesAPI {
   private async _getWSSTransport(userId: number, chatID: number): Promise<void> {
     try {
       await this._getToken(chatID);
-
       this.wssTransport = new WSTransport(`${this._baseWSSURL}/${userId}/${chatID}/${this._token}`);
     } catch (error) {
       console.log(error);
@@ -65,7 +64,7 @@ class MessagesAPI {
   }
 
   // Get messages
-  async getMessages(message: string): Promise<void> {
+  async getMessages(message: string = '0'): Promise<void> {
     try {
       if (this.wssTransport) {
         this.wssTransport.send({ content: message, type: 'get old' });
