c7d17a77a159f6ea4b5e00c4427b4c9923392858
unreal-zorro <unrealzorro@gmail.com>
2024-04-20 04:15:44 +0300

    add updateAvatar in chat-controller

--- src/controllers/chat-controller/chat-controller.ts
+++ src/controllers/chat-controller/chat-controller.ts
@@ -2,7 +2,7 @@ import { ChatAPI } from '@/api';
 import { store } from '@/store';
 import { router } from '@/router';
 import type { ChatModel } from '@/models';
-import { errorRedirect } from '@/utils';
+import { errorRedirect, validate } from '@/utils';
 
 export class ChatController {
   private chatAPI: Nullable<ChatAPI> = null;
@@ -60,6 +60,38 @@ export class ChatController {
     }
 
     return data;
+  }
+
+  public async updateAvatar(chatId: number, avatar: File) {
+    const avatarFileName = avatar.name;
+    const isValid = validate({ avatar: avatarFileName });
+
+    const data = new FormData();
+    data.append('chatId', String(chatId));
+    data.append('avatar', avatar);
+
+    let newChatData: ChatModel | undefined;
+
+    if (this.chatAPI && isValid) {
+      try {
+        newChatData = (await this.chatAPI.updateAvatar(data)) as unknown as ChatModel;
+
+        const currentChats: ChatModel[] = (await store.getState().chats) as ChatModel[];
+        const newChats = currentChats?.map((chat) =>
+          chat.id === (newChatData as unknown as ChatModel).id ? newChatData : chat
+        );
+
+        store.set('chats', newChats as ChatModel[]);
+        store.set('activeChat', undefined);
+        store.set('receivedMessages', undefined);
+      } catch (error: unknown) {
+        errorRedirect(error, router);
+      }
+    } else {
+      console.log('Invalid avatar form data');
+    }
+
+    return newChatData;
   }
 
   public async checkActiveChat(chatId: number): Promise<ChatModel> {
