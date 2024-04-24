e85e75d3f63a5e712c7c7b3def3b13f462b9f913
unreal-zorro <unrealzorro@gmail.com>
2024-04-24 18:20:28 +0300

    HTTPTransport tests added

--- src/base/HTTPTransport/HTTPTransport.spec.ts
+++ src/base/HTTPTransport/HTTPTransport.spec.ts
@@ -4,12 +4,12 @@ import type { SinonFakeXMLHttpRequestStatic, SinonFakeXMLHttpRequest } from 'sin
 import { baseURL } from '../../consts';
 import { HTTPTransport } from './HTTPTransport';
 
-describe('HTTPTransport tests', () => {
+describe('HTTPTransport', () => {
   let xhr: SinonFakeXMLHttpRequestStatic;
   let requests: SinonFakeXMLHttpRequest[];
   let transport: HTTPTransport;
 
-  before(async (done) => {
+  before(async () => {
     xhr = Sinon.useFakeXMLHttpRequest();
     global.XMLHttpRequest = xhr as unknown as {
       new (): XMLHttpRequest;
@@ -23,41 +23,73 @@ describe('HTTPTransport tests', () => {
 
     transport = new HTTPTransport(`${baseURL}`);
     requests = [];
+  });
 
+  beforeEach(async (done) => {
     xhr.onCreate = function (req) {
       requests.push(req);
     };
-
     done();
+  });
+
+  afterEach(async () => {
+    requests.length = 0;
   });
 
   after(async (done) => {
     xhr.restore();
-    requests = [];
+    requests.length = 0;
     done();
   });
 
-  it('should invoke get', () => {
+  it('should invoke get method', () => {
     transport.get('/');
 
     expect(requests[0].method).to.equal('GET');
   });
 
-  it('should invoke put', () => {
+  it('should invoke get method with params', () => {
+    transport.get('/users', { data: { name: 'user', password: '123' } });
+
+    expect(requests[0].url).to.equal(`${baseURL}/users?name=user&password=123`);
+  });
+
+  it('should invoke put method', () => {
     transport.put('/');
 
-    expect(requests[1].method).to.equal('PUT');
+    expect(requests[0].method).to.equal('PUT');
   });
 
-  it('should invoke post', () => {
+  it('should invoke put method with body', () => {
+    const data = { userId: 1 };
+    transport.put('/', { data });
+
+    expect(requests[0].requestBody).to.equal('{"userId":1}');
+  });
+
+  it('should invoke post method', () => {
     transport.post('/');
 
-    expect(requests[2].method).to.equal('POST');
+    expect(requests[0].method).to.equal('POST');
   });
 
-  it('should invoke delete', () => {
+  it('should invoke post method with withCredentials', () => {
+    const data = { withCredentials: true };
+    transport.post('/', { data });
+
+    expect(requests[0].requestBody).to.equal('{"withCredentials":true}');
+  });
+
+  it('should invoke delete method', () => {
     transport.delete('/');
 
-    expect(requests[3].method).to.equal('DELETE');
+    expect(requests[0].method).to.equal('DELETE');
+  });
+
+  it('should invoke delete method with timeout', () => {
+    const data = { timeout: 2000 };
+    transport.delete('/', { data });
+
+    expect(requests[0].requestBody).to.equal('{"timeout":2000}');
   });
 });
